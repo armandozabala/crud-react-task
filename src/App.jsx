c755d6ab5c1ea12b158fca0da07c9d6c74a87491
Armando <armandozabala@gmail.com>
2021-01-27 23:30:41 -0500

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,146 @@
+import React, { useState } from 'react';
+import shortid from 'shortid';
+
+function App() {
+
+
+  const [tarea, setTarea] = useState('')
+  const [tareas, setTareas] = useState([]);
+  const [modoEdicion, setModoEdicion] = useState(false);
+  const [id, setId] = useState('');
+  const [error, setError] = useState(null);
+
+  const editar = item => { 
+    console.log(item);
+    setModoEdicion(true);
+    setTarea(item.nombreTarea);
+    setId(item.id);
+  }
+
+  const editarTarea = e => {
+    
+      e.preventDefault();
+  
+      if (!tarea.trim()) {
+        console.log("Elemento vacio");
+        setError("Escriba algo por favor...");
+        return;
+      }
+      
+    const arrayEditado = tareas.map(
+      item => item.id === id ? { id: id, nombreTarea: tarea } : item
+    );
+
+     setTareas(arrayEditado);
+     setModoEdicion(false);
+     setTarea('');
+     setId('');
+     setError(null);
+  }
+
+  const eliminarTarea = id => {
+    const arrayFiltrado = tareas.filter(item => item.id !== id);
+    setTareas(arrayFiltrado);
+  }
+
+  const agregarTarea = e => {
+    e.preventDefault();
+   
+    if (!tarea.trim()) {
+      console.log("Elemento vacio");
+       setError("Escriba algo por favor...");
+      return;
+    }
+
+     console.log(tarea);
+     
+    setTareas([
+      ...tareas,
+      {id: shortid.generate(), nombreTarea: tarea }
+    ]);
+    
+    setTarea('');
+    setError(null);
+  }
+
+
+  return (
+    <div className="container">
+      
+       <h1 className="text-center"> CRUD Simple </h1>
+      <hr />
+      
+      <div className="row">
+          <div className="col-8">
+              <h4 className="text-center">Lista de Tareas</h4>
+          <ul className="list-group">
+
+            {
+
+              tareas.length === 0 
+                ?
+              (
+                <li className="list-group-item">No hay tarea</li>
+              )
+                :
+              (
+              tareas.map(item => (
+                <li key={item.id} className="list-group-item">
+                  <span className="lead"> {item.nombreTarea}</span>
+                  <button
+                    className="btn btn-danger btn-sm float-right mx-2"
+                    onClick={ () => eliminarTarea(item.id)}
+                  >
+                    Eliminar
+                  </button>
+                  <button
+                    className="btn btn-warning btn-sm float-right"
+                    onClick={ () => editar(item) }
+                  >
+                    Modificar</button>
+              </li>
+              ))
+              )
+            }
+           
+
+          </ul>  
+        </div>
+        <div className="col-4">
+          <h4 className="text-center">
+            Formulario { modoEdicion ? 'Editar' : 'Agregar'} </h4>
+          <form onSubmit={modoEdicion ? editarTarea : agregarTarea}>
+            
+            {
+              error ? <span className="text-danger"> { error }</span> : null
+            }
+
+            <input
+              type="text"
+              className="form-control mb-2"
+              placeholder="Ingrese Tarea"
+              onChange={e => setTarea(e.target.value)}
+              value={tarea}
+            />
+
+            {
+              modoEdicion
+              ?
+              (
+               <button className="btn btn-warning btn-block" type="submit">Editar</button>
+              )
+              :
+              (
+                 <button className="btn btn-dark btn-block" type="submit">Agregar</button>
+            )}
+           
+          </form>
+         </div>
+      </div>
+       
+
+    </div>
+  );
+}
+
+export default App;
